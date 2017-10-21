525c68577b71175d465a8a0e7e10d4960b3f04db
markdyousef <markdyousef@gmail.com>
2017-10-21 14:16:08 +0200

    isolated lungs on ct-scans using kmeans

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+from skimage import morphology, measure
+from skimage.transform import resize
+from sklearn.cluster import KMeans
 
 def make_mask(center, diameter, channels, width, height, spacing, origin):
     '''
@@ -31,3 +34,60 @@ def make_mask(center, diameter, channels, width, height, spacing, origin):
                 mask[int((p_y-origin[1]) / spacing[1]), int((p_x-origin[0]) / spacing[0])] = 1.0
 
     return mask
+
+
+def filter_lungs(prop):
+    # background is removed by removing regions with a bbox to large
+    # on either dimension
+    # lungs are usually centered - remove regions to close to top and bottom
+    bbox = prop.bbox
+    if bbox[2]-bbox[0]-475 and bbox[3]-bbox[1]<475 and bbox[0]>40 and bbox[2]<472:
+        return prop.label
+
+def isolate_lungs(image_file):
+    # load as float64 to work with scipy.kmeans
+    imgs = np.load(image_file).astype(np.float64)
+    for i in range(len(imgs)):
+        img = imgs[i]
+        # standardize px values
+        mean = np.mean(img)
+        std = np.std(img)
+        img = (img-mean)/std
+        # find avg px value near lungs (centered in scan)
+        middle = img[100:400, 100:400]
+        img_mean = np.mean(middle)
+        img_max = np.max(img)
+        img_min = np.min(img)
+        # move underflow and overflow on px spectrum
+        # improves threshold finding
+        img[img==img_max]=mean
+        img[img==img_min]=mean
+        # kmeans for separating foreground (radio-opaque tissue)
+        # and background (radio transparent tissue ie lungs)
+        kmeans = KMeans(n_clusters=2).fit(np.reshape(middle, [np.prod(middle.shape), 1]))
+        centers = sorted(kmeans.cluster_centers_.flatten())
+        threshold = np.mean(centers)
+        thresh_img = np.where(img < threshold, 1.0, 0.0)
+        # initial erosion to remove graininess for some regions
+        # large dialation to make lung region engulf the vessels
+        # and incursions into the lung cavity by radio-opaque tissue
+        eroded = morphology.erosion(thresh_img, np.ones([4, 4]))
+        dilation = morphology.dilation(eroded, np.ones([10, 10]))
+
+        # label regions and obtain region properties
+        labels = measure.label(dilation)
+        regions = measure.regionprops(labels)
+        # retrieve labels for lungs
+        good_labels = [filter_lungs(prop) for prop in regions]
+        # mask lungs
+        mask = np.ndarray([512, 512], dtype=np.int8)
+        mask[:] = 0
+        for N in good_labels:
+            mask = mask + np.where(labels==N, 1, 0)
+        # large dilation to fill out lung mask
+        mask = morphology.dilation(mask, np.ones([10, 10]))
+        imgs[i] = mask
+
+    np.save(image_file.replace('images', 'lungmask'), imgs)
+
+            
